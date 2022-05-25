93fed7b92d6a05f63a92c545e263ff65c27651a1
SONU-TOMAR <104898655+IamSonuTomar@users.noreply.github.com>
2022-05-25 14:24:46 +0530

    Add files via upload

---
+++ 28march.py
@@ -0,0 +1,57 @@
+#print('hello')
+
+
+#add =
+'''x=5
+y=6
+z=x+y
+print(z)'''
+
+
+'''def expo (a,b):
+	x=a**b
+	print (x)
+def add(c,d):
+	y=c+d
+	print (y)
+expo(2,4)2
+
+add(2,5)
+expo(5,6)'''
+
+
+'''a=int(input("enter  1st no : "))
+b=int(input("enter 2nd no: "))
+def mod():
+	x=a%b
+	print(x) 
+def div():
+	y=a/b
+	print(y)
+
+
+
+mod()
+div()'''
+
+'''class x:
+	i=5
+p=x()
+print(p.i)
+
+
+t=x()
+print(t.i)'''
+'''class x:
+	a=6
+	b=7
+	z=a+b
+
+f=x()
+print(f.z)'''
+
+class x:
+	def z(self,a,b):
+		print(a+b)
+e=x()
+e.z(4,6)
